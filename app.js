8fb5767d9d3c3b94d0a040904b83ce0840eb4ac9
Sergio <sergiodanielxd2004@gmail.com>
2023-11-21 01:51:20 -0600

    Creacion de base de datos. Permitir el ingreo de informacion en registro

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ import ejs from "ejs";
 import express from "express";
 import pg from "pg";
 
-
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+    host: 'localhost',
+    port: 5432,
+    database: 'Quizzin',
+    password: '123456',
+    user: 'postgres'
+  });
+
+db.connect();
 
 app.use(express.static("public"));
 
@@ -17,15 +26,22 @@ app.get("/", (req,res) =>{
     res.render("index.ejs");
 });
 
-app.get("/register", (req,res) =>{
+app.get("/register", async (req,res) =>{
     res.render("register.ejs");
+
 });
 
 app.get("/login", (req,res) =>{
     res.render("login.ejs");
 });
 
+app.post("/register", async (req,body)=>{
+    const nombreUsuario = req.body.nombre;
+    const correo = req.body.correo;
+    const contra = req.body.contrasena;
 
+    await db.query("INSERT INTO usuario(nombreUsuario, correo, contra) VALUES($1, $2, $3)", [nombreUsuario, correo, contra]);
+});
 app.listen(port, ()=>{
     console.log("conectado");
 });
